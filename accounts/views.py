27f93e0259ab1316ca7e9d55179e07c550925656
Sherdil <57909176+Sherdil28@users.noreply.github.com>
2024-06-10 13:37:20 +0530

    Initial setup

---
+++ accounts/views.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+
+from accounts.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
